95e4ac2f02d8cc9f7545a8b690f2b8fe0eb93b9b
Anguer <191030830@qq.com>
2024-03-17 23:26:38 +0800

    :wrench: chore: small fix

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,4 +2,6 @@ export function test(num: number, num2: number) {
   console.log('#test func:', num * num2);
 }
 
-export function test2(c: number) {}
+export function test2(num: number, num2: number) {
+  console.log('#test func:', num * num2);
+}
