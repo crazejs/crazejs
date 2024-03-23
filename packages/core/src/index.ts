89230f6b173061c8df41e45079dd21491ea783ac
Anguer <191030830@qq.com>
2024-03-23 19:26:37 +0800

    :bug: fix(core): small fix

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,12 @@
 export function test(num: number, num2: number) {
   console.log('#test func:', num * num2);
+  return num * num2;
 }
 
 export function test2(num: number, num2: number) {
-  console.log('#test func:', num * num2);
+  console.log('#test func:', num + num2);
+  return num + num2;
 }
+
+console.log(test(1, 2));
+console.log('done.');
