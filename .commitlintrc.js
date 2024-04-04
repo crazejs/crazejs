30fe0855bf5c750c14440df22abf634dfb78017f
Anguer <191030830@qq.com>
2024-04-04 20:45:27 +0800

    :wrench: chore: update .commitlintrc

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
-  // ...require('@crazejs/lint').commitlint,
-  $schema: "https://json.schemastore.org/commitlintrc",
-  extends: ["gitmoji"],
-  rules: {
-    "footer-leading-blank": [0, "never"],
-    "header-max-length": [0, "never"]
-  },
+  ...require('@crazejs/lint').commitlint,
   ignores: [(commit) => /^Version Packages(.*)/.test(commit)],
 };
